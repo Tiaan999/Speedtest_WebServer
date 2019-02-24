25e4ae6d2b96e4c53fc658a480a884243f3de593
Tiaan Willemse <Tiaan999@users.noreply.github.com>
2019-02-24 15:55:16 -0500

    Added file exists check.
    Creates a new log file and adds data header to the first line if it doesn't.

--- log_speed.sh
+++ log_speed.sh
@@ -1,5 +1,13 @@
 #! /bin/bash
 
+if [ -s speedtest_data.log ]
+then
+	:
+else
+	touch speedtest_data.log
+	speedtest --csv-header >> speedtest_data.log
+fi
+
 while true;
 do
 	speedtest --csv >> speedtest_data.log
