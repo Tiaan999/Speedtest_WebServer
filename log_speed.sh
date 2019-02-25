2de22f246372b83f86c9d21eecdc658a3e2b30ce
Tiaan999 <tiaan1.willemse@gmail.com>
2019-02-24 20:45:11 -0500

    no message

--- log_speed.sh
+++ log_speed.sh
@@ -12,7 +12,7 @@ while true;
 do
 	echo "Measuring speed..."
 	speedtest --csv >> speedtest_data.log
-	time_now = "'date +%H:%M:%S'";
+	time_now = "$(date +'%H:%M:%S')";
 	echo "Speed measured at $time_now"
 	sleep 60
 done
